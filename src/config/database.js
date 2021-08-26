7cd5bdd546afed9a558ea4bc6e313f4bc1a81e7c
BẢO ĐINH MINH <19120173@student.hcmus.edu.vn>
2021-08-26 15:54:25 +0700

    Make it better

--- src/config/database.js
+++ src/config/database.js
@@ -8,7 +8,7 @@ const config = process.env.DATABASE_URL;
 
 const pool = new Pool({
     connectionString: config,
-    /* ssl: { rejectUnauthorized: false } */ // deploy uncomment
+    ssl: { rejectUnauthorized: false }
 });
 
 pool.on('connect', () => {
