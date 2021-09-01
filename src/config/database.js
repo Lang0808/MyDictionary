2dda7682acc5ad9a5eb208083d31487dca139e06
BẢO ĐINH MINH <19120173@student.hcmus.edu.vn>
2021-09-01 08:38:50 +0700

    Deploy

--- src/config/database.js
+++ src/config/database.js
@@ -8,7 +8,7 @@ const config = process.env.DATABASE_URL;
 
 const pool = new Pool({
     connectionString: config,
-    /*ssl: { rejectUnauthorized: false } */
+    ssl: { rejectUnauthorized: false }
 });
 
 pool.on('connect', () => {
